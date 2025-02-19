2b868f7e60e6f61ed2605526e793d305398892e5
Anuroop-k-das <106763669+Anuroop-k-das@users.noreply.github.com>
2025-02-19 17:10:48 +0530

    Updated qrs complex quiz

--- lib/topics_completion_tracker/qrs_complex_quiz_screen.dart
+++ lib/topics_completion_tracker/qrs_complex_quiz_screen.dart
@@ -43,29 +43,60 @@ class _QRSComplexQuizScreenState extends State<QRSComplexQuizScreen> {
       ],
       'answer': 2,
     },
+    {
+      'question': 'Which lead primarily assesses QRS axis deviation?',
+      'options': ['Lead I', 'Lead II', 'Lead aVF', 'Lead III'],
+      'answer': 0,
+    },
+    {
+      'question': 'What does a notched QRS complex suggest?',
+      'options': ['Ventricular hypertrophy', 'Myocardial infarction', 'Bundle branch block', 'Atrial fibrillation'],
+      'answer': 2,
+    },
+    {
+      'question': 'What causes pathological Q waves in the QRS complex?',
+      'options': ['Hyperkalemia', 'Myocardial infarction', 'Atrial flutter', 'Pericarditis'],
+      'answer': 1,
+    },
+    {
+      'question': 'Which electrolyte imbalance can prolong the QRS duration?',
+      'options': ['Hypokalemia', 'Hyperkalemia', 'Hyponatremia', 'Hypercalcemia'],
+      'answer': 1,
+    },
+    {
+      'question': 'A tall R wave in V1 suggests?',
+      'options': ['Right bundle branch block', 'Left bundle branch block', 'Hyperkalemia', 'Pericarditis'],
+      'answer': 0,
+    },
+    {
+      'question': 'What does a narrow QRS complex indicate?',
+      'options': ['Supraventricular origin', 'Ventricular origin', 'Left atrial enlargement', 'Right atrial enlargement'],
+      'answer': 0,
+    },
+    {
+      'question': 'A delta wave in the QRS complex is associated with?',
+      'options': ['WPW syndrome', 'AV block', 'Atrial fibrillation', 'PEA arrest'],
+      'answer': 0,
+    }
   ];
 
   void _checkAnswer(int selectedIndex) {
     if (selectedIndex == questions[currentQuestionIndex]['answer']) {
       score++;
-      setState(() {
-        if (currentQuestionIndex < questions.length - 1) {
-          currentQuestionIndex++;
-        } else {
-          _showCompletionDialog();
-        }
-      });
-    } else {
-      setState(() {
-        showResetButton = true;
-      });
     }
+    setState(() {
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+      } else {
+        _showCompletionDialog();
+      }
+    });
   }
 
   Future<void> _storeCompletionStatus() async {
     try {
       User? user = FirebaseAuth.instance.currentUser;
-      if (user != null && score == questions.length) {
+      if (user != null && score >= 8) {
         String uid = user.uid;
         DocumentReference userDoc = FirebaseFirestore.instance.collection('userProgress').doc(uid);
 
@@ -79,20 +110,25 @@ class _QRSComplexQuizScreenState extends State<QRSComplexQuizScreen> {
   }
 
   void _showCompletionDialog() {
+    bool passed = score >= 8;
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Quiz Completed!'),
+          title: Text(passed ? 'Quiz Passed!' : 'Quiz Failed'),
           content: Text('You scored $score out of ${questions.length}.'),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
-                Navigator.of(context).pop();
-                _storeCompletionStatus();
+                if (passed) {
+                  Navigator.of(context).pop();
+                  _storeCompletionStatus();
+                } else {
+                  _resetQuiz();
+                }
               },
-              child: Text('Finish'),
+              child: Text(passed ? 'Finish' : 'Retry'),
             ),
           ],
         );
@@ -121,51 +157,37 @@ class _QRSComplexQuizScreenState extends State<QRSComplexQuizScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (!showResetButton) ...[
-              Text(
-                'Question ${currentQuestionIndex + 1} of ${questions.length}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+            Text(
+              'Question ${currentQuestionIndex + 1} of ${questions.length}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 16),
-              Text(
-                questions[currentQuestionIndex]['question'],
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              questions[currentQuestionIndex]['question'],
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-              SizedBox(height: 16),
-              ...List.generate(questions[currentQuestionIndex]['options'].length, (index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey[800],
-                      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-                      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () => _checkAnswer(index),
-                    child: Text(questions[currentQuestionIndex]['options'][index]),
-                  ),
-                );
-              }),
-            ] else ...[
-              Center(
+            ),
+            SizedBox(height: 16),
+            ...List.generate(questions[currentQuestionIndex]['options'].length, (index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
+                    backgroundColor: Colors.white,
                     padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-                    textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
-                  onPressed: _resetQuiz,
-                  child: Text('Restart Quiz'),
+                  onPressed: () => _checkAnswer(index),
+                  child: Text(questions[currentQuestionIndex]['options'][index]),
                 ),
-              ),
-            ],
+              );
+            }),
           ],
         ),
       ),
